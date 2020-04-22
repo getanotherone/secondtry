befff3206b6f1c09a9e7e030ec2a09418c10e786
Andrei Galai <po4kinz@gmail.com>
2020-04-22 19:24:30 +0300

    ver. 2

--- 22.04.20/22.04.20.py
+++ 22.04.20/22.04.20.py
@@ -1,7 +1,11 @@
+# for i in range(1, 11):
+#    number = []
+#    for j in range(10):
+#        number.append(i)
+#    for k in range(1, 11):
+#        number[k-1] *= k
+#    print(number)
+
 for i in range(1, 11):
-    number = []
-    for j in range(10):
-        number.append(i)
-    for k in range(1, 11):
-        number[k-1] *= k
+    number = [value * 2 for value in range(1, 11)]
     print(number)
